051546f7e5008a4dd6ca0858d0e82dd67e6b86fe
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2025-11-29 20:42:00 -0400

    organizar carpetas Rev 1

--- 02-arrays/app.js
+++ 02-arrays/app.js
@@ -1,1 +1,35 @@
 console.log("Arrays")
+
+let frutas = ["limón", "mango", "fresa"];
+const personajes = ["Rick", "Morty", "Beth"];
+
+// console.log(frutas)
+// frutas = ["moras"]
+// console.log(frutas)
+
+console.log(personajes);
+console.log(personajes[1]);
+
+
+personajes[1] = "Jerry";
+console.log(personajes[1]);
+console.log(personajes.length);
+
+// for (let i=0; i < personajes.length; i++) {
+//     console.log(personajes[i]); //Accede al elemento en la posicion i
+    
+// }
+
+console.log(frutas);
+frutas.push("banano");
+console.log(frutas);
+frutas.unshift("coco");
+console.log(frutas);
+
+/* ctrl + / (múltiples comentarios de una sola línea) */
+/* alt + shift + a (comentario de bloque) */
+
+// console.log(frutas)
+// frutas = ["moras"]
+
+//pag 18
